a12858d9fc3e537499eb4ef15de3b68fa35fda0e
Matt Roberts <sweetcrapmatt@gmail.com>
2022-01-22 10:47:07 -0600

    Initial Commit part 2.

---
+++ src/main/java/us/mattroberts/weedbuddy/WeedBuddyApplication.java
@@ -0,0 +1,13 @@
+package us.mattroberts.weedbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeedBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeedBuddyApplication.class, args);
+	}
+
+}
